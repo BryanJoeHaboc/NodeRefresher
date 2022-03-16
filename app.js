e62ca97b7002a4250e6b3e3cd8292caaccad1a02
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-03-16 22:29:57 +0800

    added body Parser to parse body

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 // root file of node js
-const http = require("http");
-const { requestHandler } = require("./routes");
 
-const server = http.createServer(requestHandler);
+const express = require("express");
 
-server.listen(3000);
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product"  method="POST" /> <input type="text" name="title"> <button type="submit">Add Product</button> </form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  const { body } = req;
+
+  res.send(`<h1>Hello world</h1>`);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
