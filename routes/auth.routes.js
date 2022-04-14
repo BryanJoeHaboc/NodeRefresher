91cb65db258b76f6f5263f4bbf819222e69b65bd
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-04-14 19:21:27 +0800

    deleted routes that has redirect

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,10 @@
 const express = require("express");
 
 const {
-  getLogin,
   postLogin,
   postLogout,
-  getSignUp,
   postSignUp,
-  getResetPassword,
   postResetPassword,
-  getNewPassword,
   postNewPassword,
 } = require("../controllers/auth.controller");
 
@@ -19,22 +15,10 @@ const {
 
 const router = express.Router();
 
-router.get("/login", getLogin);
-
-router.get("/signup", getSignUp);
-
 router.post("/login", postLoginValidator, postLogin);
-
 router.post("/signup", postSignUpValidator, postSignUp);
-
-router.post("/logout", postLogout);
-
-router.get("/reset", getResetPassword);
-
+// router.post("/logout", postLogout);
 router.post("/reset", postResetPassword);
-
-router.get("/reset/:token", getNewPassword);
-
 router.post("/new-password", postNewPassword);
 
 module.exports = router;
