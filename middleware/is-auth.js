deabedb21332a1f402de595958d9322f7e978a0f
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-04-15 19:45:45 +0800

    refactor: added jwt to is-auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,39 @@
+const jwt = require("jsonwebtoken");
+
+const passToErrorMiddleware = (err, next) => {
+  console.log("err", err);
+  if (!err.statusCode) {
+    err.statusCode = 500;
+  }
+  next(err);
+};
+
 const checkIfAuthenticated = (req, res, next) => {
-  if (!req.session.isLoggedIn) return res.redirect("/login");
+  try {
+    const authHeader = req.get("Authorization");
+
+    if (!authHeader) {
+      const error = new Error("Not authenticated");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = authHeader.split(" ")[1];
+    let decodedToken;
+
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decodedToken) {
+      const error = new Error("Not authenticated.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    req.userId = decodedToken.userId;
+  } catch (err) {
+    passToErrorMiddleware(err, next);
+  }
+
   next();
 };
 
