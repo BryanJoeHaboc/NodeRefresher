90a4a1d24ec2ce10a8f2200597e4ced60f0ead71
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-05-18 23:12:40 +0800

    initial test

--- test/start.test.js
+++ test/start.test.js
@@ -19,26 +19,5 @@ function createSignUp({
 const res = () => {};
 const next = () => {};
 describe("Auth Controller", () => {
-  it("throws error if password and confirmPassword not equal", () => {
-    const req = {
-      body: createSignUp("", "", "", "pass", "password"),
-    };
-
-    console.log(req);
-    expect(postSignUp(req, res, next())).toStrictEqual({
-      error: "Password and confirm password must be equal",
-    });
-  });
-
-  // it("throws error if email already exists", () => {});
-
-  // it("throws error if firstName is empty", () => {});
-
-  // it("throws error if lastName is empty", () => {});
-
-  // it("throws error if password  > 6 characters ", () => {});
-
-  // it("throws error if password is not equal to confirmpassword", () => {});
-
-  // it("creates valid user", () => {});
+  it("throws error if user inputted invalid credentials", () => {});
 });
