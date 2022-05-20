d2f04c1fa6e7d65e48d5a193bdf8976c210a2967
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-05-20 22:44:26 +0800

    refactor: added try catch to accessSecretVersion

--- secretManager.js
+++ secretManager.js
@@ -6,18 +6,22 @@ const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
 const client = new SecretManagerServiceClient();
 
 async function accessSecretVersion(name) {
-  const [version] = await client.accessSecretVersion({
-    name: name,
-  });
+  try {
+    const [version] = await client.accessSecretVersion({
+      name: name,
+    });
 
-  // Extract the payload as a string.
-  const payload = version.payload.data.toString();
+    // Extract the payload as a string.
+    const payload = version.payload.data.toString();
 
-  // WARNING: Do not print the secret in a production environment - this
-  // snippet is showing how to access the secret material.
-  //   console.info(`Payload: ${payload}`);
+    // WARNING: Do not print the secret in a production environment - this
+    // snippet is showing how to access the secret material.
+    //   console.info(`Payload: ${payload}`);
 
-  return payload;
+    return payload;
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 module.exports = {
