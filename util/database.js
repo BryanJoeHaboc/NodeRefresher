a4e604b6b137b90624155fbb7900aded992388e7
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-05-22 00:29:46 +0800

    refactor: added socketPath in mysqlconfig in production

--- util/database.js
+++ util/database.js
@@ -5,24 +5,32 @@ const { accessSecretVersion } = require("../secretManager");
 let dbPw = "";
 let dbHost = "";
 
+const mysqlConfig = {
+  dialect: "mysql",
+  host: dbHost,
+};
+
 function getSecretFromGoogle() {
   if (process.env.NODE_ENV === "production") {
+    console.log("pumasok sa production");
     dbPw = accessSecretVersion(
       "projects/65293551526/secrets/ECOMMERCE_DB_PW_ENV/versions/latest"
     );
-    dbHost = accessSecretVersion(
-      "projects/65293551526/secrets/ECOMMERCE_DB_HOST_ENV/versions/latest"
+    const instanceConnectionName = accessSecretVersion(
+      "projects/65293551526/secrets/ECOMMERCE_INSTANCE_CONNECTION_NAME/versions/latest"
     );
+    mysqlConfig.host = "localhost";
+    mysqlConfig.dialectOptions = {
+      socketPath: `/cloudsql/${instanceConnectionName}`,
+    };
   } else {
+    console.log("pumasok sa deployment");
     dbPw = process.env.DB_PW_DEV;
-    dbHost = process.env.DB_HOST_DEV;
+    mysqlConfig.host = process.env.DB_HOST_DEV;
   }
 }
 getSecretFromGoogle();
 
-const sequelize = new Sequelize("e-commerce", "root", dbPw, {
-  dialect: "mysql",
-  host: dbHost,
-});
+const sequelize = new Sequelize("e-commerce", "root", dbPw, mysqlConfig);
 
 module.exports = sequelize;
